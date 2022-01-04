806d44393fb2e16ed5cc22fcbc69abb7b0c87280
boodscode237 <65740750+boodscode237@users.noreply.github.com>
2022-01-04 17:05:53 +0500

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SayAuthor();
+
+            string firstName = GetUserName("What is your first name: ");
+            string lastName = GetUserName("What is your last name: ");
+
+            WelcomeUser(firstName, lastName);
+
+            Console.ReadLine();
+        }
+
+        private static void SayAuthor()
+        {
+            Console.WriteLine("*****************************");
+            Console.WriteLine("Written By: Brice Donald");
+            Console.WriteLine("In his way to C# mastering");
+            Console.WriteLine("*****************************");
+        }
+
+
+        private static void WelcomeUser(string firstName, string lastName)
+        {
+            Console.WriteLine($"Hello {firstName} {lastName}");
+        }
+
+        private static string GetUserName(string message)
+        {
+            Console.Write(message);
+            string output = Console.ReadLine();
+
+            if (output.Length > 1)
+            {
+                return output;
+            }
+            else
+            {
+                return "UNKNOWN";
+            }
+        }
+
+
+    }
+}
